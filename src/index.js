6d836c2f5660505fe0a0cfe386f4afbf9fa2af8b
Brian Purnell <purnellbp@gmail.com>
2020-06-18 14:59:30 -0500

    components completed

--- src/index.js
+++ src/index.js
@@ -2,21 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from "./CommentDetail";
 import ApprovalCard from "./ApprovalCard";
+import Segment from "./Segment";
 import faker from "faker";
 
+// ### Components Practice ###
 const App = () => {
     return (
-        <div className="ui container comments">
-            <ApprovalCard>
-                <CommentDetail
-                    author={faker.internet.userName()}
-                    timeAgo="Today"
-                    content={faker.lorem.sentence()}
-                    avatarImage={faker.image.avatar()}
-                />
-            </ApprovalCard>
-        </div>
+        <Segment>
+            <div className="ui container comments">
+                <ApprovalCard>
+                    <div><h4>WARNING!</h4> Are you sure you want to do this?</div>
+                </ApprovalCard>
+                <ApprovalCard>
+                    <CommentDetail
+                        author={faker.internet.userName()}
+                        timeAgo="Today"
+                        content={faker.lorem.sentence()}
+                        avatarImage={faker.image.avatar()}
+                    />
+                </ApprovalCard>
+            </div>
+        </Segment>
     );
 };
+
 
 ReactDOM.render(<App/>, document.querySelector('#root'))
